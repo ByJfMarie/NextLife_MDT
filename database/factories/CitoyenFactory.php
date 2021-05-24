34b67da146044e443d4168faf7adae7214bef440
Jean-françois Marie <jfdu50@outlook.fr>
2021-05-24 22:43:14 +0200

    table citoyuen

---
+++ database/factories/CitoyenFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Citoyen;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CitoyenFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Citoyen::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_05_24_163352_create_citoyens_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitoyensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citoyens', function (Blueprint $table) {
+            $table->id();
+            $table->text('nom');
+            $table->text('prenom');
+            $table->date('dateDeNaissance');
+            $table->text('civilite');
+            $table->text('type');
+            $table->text('adresse');
+            $table->text('telephone');
+            $table->binary('photo');
+            $table->boolean('driveLicense')->default(false);
+            $table->boolean('weaponLicense')->default(false);
+            $table->boolean('isWanted')->default(false);
+            $table->boolean('isSummoned')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citoyens');
+    }
+}
