62f8a53d4c9667c882f9ae6764342dc9e005ed81
ByJfMarie <jf.marie@outlook.fr>
2021-08-03 12:05:08 +0200

    recherche citoyen + coffre

--- resources/js/pages/TAJ.vue
+++ resources/js/pages/TAJ.vue
@@ -2,23 +2,25 @@
     <div>
         <div class="content">
 
-            <div class="serach-container">
-                <h1>heelo</h1>
+            <div class="search-container">
+                    <router-link to="/taj/create" class="nav-item nav-link">Ajouter Casier</router-link>
+                    <router-link to="/rapport-arrestation" class="nav-item nav-link">Rapport D'arrestation</router-link>
+                    <router-link to="/taj/amende" class="nav-item nav-link">Créer une amende</router-link>
+                    <router-link to="add-coffre" class="nav-item nav-link">Ajouter au coffre des saisies</router-link>
+                
             </div>
 
             <div class="right">
                 
                 <div class="buttons">
-                    <router-link to="/taj/create" class="nav-item nav-link">Ajouter</router-link>
-                    <router-link to="/rapport-arrestation" class="nav-item nav-link">Rapport D'arrestation</router-link>
-                    <router-link to="/taj/amende" class="nav-item nav-link">Créer une amende</router-link>
+                    <input class="nav-item-input search" type="search" placeholder="Recherche Citoyen (nom et/ou prénom)" name="" v-model="searchKey" id="search">
                     <router-link to="/dashboard" class="logout">Retour Dashboard</router-link>
                 </div>
                 
                 <div class="cards-container">
                     
                     
-                    <div class="card" v-for="data in datas" v-on:click="goToCasier(data.id)">
+                    <div class="card" v-for="data in filteredList" v-on:click="goToCasier(data.id)">
                         <!-- <div class="image-container">
                             <img :src="`/storage/${data.photo_path}`" alt="">
                         </div> -->
@@ -40,6 +42,7 @@ export default {
     data() {
         return {
             datas: null,
+            searchKey: ''
         }
     },
     created() {
@@ -53,6 +56,16 @@ export default {
     methods: {
         goToCasier(data){
             window.location.href = "/taj/" + data;
+        }
+    },
+    computed: {
+        filteredList() {
+            if (this.datas !== null) {
+                 return this.datas.filter((data) => {
+                    var name = data.prenom + " " + data.nom;
+                    return name.toLowerCase().includes(this.searchKey.toLowerCase());
+                })
+            }
         }
     },
     beforeRouteEnter(to, from, next) {
